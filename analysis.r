fd4570675c24108c12f57dbccc1a884dbf2a9087
rohan aras <rohanaras@gmail.com>
2015-06-25 14:54:10 -0700

    software carpentry easy r tutorial

---
+++ analysis.r
@@ -0,0 +1,6 @@
+# setting directory and reading in a file 
+setwd("/R_and_Python_data_files/")
+inflamation_data = read.csv('inflammation-01.csv', header = FALSE)
+
+weightKilo <- 55
+weightLbs <- 2.2 * weightKilo
